719d262bccc4e703d71f8c536a99ee2119336809
vaibhavrc1 <51832045+vaibhavrc1@users.noreply.github.com>
2020-05-17 17:16:47 +0530

    Update Calculator.java

--- Bank/src/application/Calculator.java
+++ Bank/src/application/Calculator.java
@@ -5,6 +5,8 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
+//Note:Everything inside TextField and label is in String format, therefore we are converting them into usable format.
+
 public class Calculator {
 	@FXML
 	Button calSI;
@@ -18,14 +20,14 @@ public class Calculator {
 	TextField tr;
 	@FXML
 	TextField tt;
-	public void simpleInterest() {
+	public void simpleInterest() {    //calculating simple interest
 		float p=Float.parseFloat(tp.getText());
 		float r=Float.parseFloat(tr.getText());
 		float t=Float.parseFloat(tt.getText());
 		float SI=p*r*t/100;
 		lOutput.setText(Float.toString(SI));
 	}
-	public void clear() {
+	public void clear() {            //clearing inputs to 0
 		tp.setText("");
 		tr.setText("");
 		tt.setText("");
