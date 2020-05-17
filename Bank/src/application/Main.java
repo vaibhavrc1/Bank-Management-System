7ce2c4d123ed21d2b5cc4f5c0344a79808abd6d2
vaibhavrc1 <51832045+vaibhavrc1@users.noreply.github.com>
2020-05-17 17:13:27 +0530

    Update Main.java

--- Bank/src/application/Main.java
+++ Bank/src/application/Main.java
@@ -8,22 +8,22 @@ import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
 
-public class Main extends Application {
+public class Main extends Application { //important to extend Main class with Application for using GUI.
 	@Override
-	public void start(Stage primaryStage) {
+	public void start(Stage primaryStage) {   //compulsory to use this method after extending class "Application" 
 		try {
-			Parent root=FXMLLoader.load(getClass().getResource("SI GUI.fxml"));
-			Scene scene = new Scene(root,500,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.setTitle("Simple Interest Calculator");
-			primaryStage.show();
+			Parent root=FXMLLoader.load(getClass().getResource("SI GUI.fxml"));//loading FXML file
+			Scene scene = new Scene(root,500,400);  //setting width and height of the window 
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm()); //loading CSS file if used
+			primaryStage.setScene(scene);  // scene=content inside window
+			primaryStage.setTitle("Simple Interest Calculator"); //Title of the window
+			primaryStage.show();  //Showing window to user
 		} catch(Exception e) {
-			e.printStackTrace();
+			e.printStackTrace();  //Printing Exception
 		}
 	}
 	
 	public static void main(String[] args) {
-		launch(args);
+		launch(args);  //Starting GUI
 	}
 }
